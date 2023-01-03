1de3303a81f3b1eacf554efbb8d1ba1fc979836b
0xkongamoto <hieu.q@autonomous.nyc>
2023-01-03 09:25:54 +0700

    empty source

--- contracts/admin/IMultipleUpgradeableProxy.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.8.0;
-
-import {IPawnProxyAdmin} from "./IPawnProxyAdmin.sol";
-import {Proxy} from "../openzeppelin/contracts/proxy/Proxy.sol";
-
-interface IMultipleUpgradeableProxy {
-    function multipleProxyKey() external view returns (bytes32);
-}
--- migrations/upgrade_contract.js
+++
@@ -1,6 +0,0 @@
-module.exports = async function (pawnProxyAdmin, proxyAddress, implAddress) {
-    if ((await pawnProxyAdmin.getProxyImplementation(proxyAddress)).toString() != implAddress) {
-        await pawnProxyAdmin.upgrade(proxyAddress, implAddress);
-        console.log('pawnProxyAdmin.upgrade()', proxyAddress, implAddress)
-    }
-};
