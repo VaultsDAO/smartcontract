780c79f241a85088524aa8627d042567eb102995
0xkongamoto <hieu.q@autonomous.nyc>
2023-01-19 13:00:43 +0700

    rf

--- deployments/25_migrate_Trade.ts
+++ deployments/25_migrate_Trade.ts
@@ -116,8 +116,8 @@ async function main() {
         let markTwap = new bn(formatSqrtPriceX96ToPrice(markTwapX96, 18))
         let indexPrice = new bn(formatEther((await baseToken.getIndexPrice(0))))
         // let deltaPrice = markTwap.minus(indexPrice).abs()
-        let rndInt = Math.floor(Math.random() * 1000000) % 5 + 1
-        let deltaPrice = (indexPrice.multipliedBy(0.008 * rndInt))
+        let rndInt = Math.floor(Math.random() * 1000000) % 10 + 1
+        let deltaPrice = (indexPrice.multipliedBy(0.05 * rndInt))
         let tradeAmount = deltaPrice.div(indexPrice).multipliedBy(1000).div(indexPrice).div(2)
         console.log(
             baseToken.address,
