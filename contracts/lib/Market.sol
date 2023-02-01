a5df47535737e5605e860ccf41140f125ca42d8f
0xkongamoto <hieu.q@autonomous.nyc>
2023-02-01 13:30:07 +0700

    f

--- contracts/lib/Market.sol
+++ contracts/lib/Market.sol
@@ -4,6 +4,8 @@ pragma solidity 0.7.6;
 library Market {
     /// @param lastTwPremiumGrowthGlobalX96 the last time weighted premiumGrowthGlobalX96
     struct Info {
+        uint256 longMultiplierX10_18; //X10_18
+        uint256 shortMultiplierX10_18; //X10_18
         uint256 longPositionSize;
         uint256 shortPositionSize;
     }
