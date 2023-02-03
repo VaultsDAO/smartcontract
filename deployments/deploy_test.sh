fba2fcbf2b5c6e7b2fb14db59d22ef2bb9d18900
0xkongamoto <hieu.q@autonomous.nyc>
2023-02-03 16:10:17 +0700

    rf deploy

--- deployments/deploy_test.sh
+++ deployments/deploy_test.sh
@@ -1,31 +1,61 @@
 # npm run build
 
-npx hardhat run deployments/1_migrate_Admin.ts --network arbitrumGoerli --no-compile
-npx hardhat run deployments/2_migrate_PriceFeed_1BAYC.ts --network arbitrumGoerli --no-compile
-npx hardhat run deployments/2_migrate_PriceFeed_2MAYC.ts --network arbitrumGoerli --no-compile
-npx hardhat run deployments/3_migrate_Tokens.ts --network arbitrumGoerli --no-compile
-npx hardhat run deployments/4_migrate_QuoteToken.ts --network arbitrumGoerli --no-compile
-npx hardhat run deployments/5_migrate_BaseToken_0.ts --network arbitrumGoerli --no-compile
-npx hardhat run deployments/5_migrate_BaseToken_1BAYC.ts --network arbitrumGoerli --no-compile
-npx hardhat run deployments/5_migrate_BaseToken_2MAYC.ts --network arbitrumGoerli --no-compile
-npx hardhat run deployments/6_migrate_UniswapV3.ts --network arbitrumGoerli --no-compile
-npx hardhat run deployments/7_migrate_ClearingHouseConfig.ts --network arbitrumGoerli --no-compile
-npx hardhat run deployments/8_migrate_MarketRegistry.ts --network arbitrumGoerli --no-compile
-npx hardhat run deployments/9_migrate_OrderBook.ts --network arbitrumGoerli --no-compile
-npx hardhat run deployments/10_migrate_AccountBalance.ts --network arbitrumGoerli --no-compile
-npx hardhat run deployments/11_migrate_Exchange.ts --network arbitrumGoerli --no-compile
-npx hardhat run deployments/12_migrate_InsuranceFund.ts --network arbitrumGoerli --no-compile
-npx hardhat run deployments/13_migrate_Vault.ts --network arbitrumGoerli --no-compile
-npx hardhat run deployments/14_migrate_CollateralManager.ts --network arbitrumGoerli --no-compile
-npx hardhat run deployments/15_migrate_ClearingHouse.ts --network arbitrumGoerli --no-compile
-npx hardhat run deployments/16_migrate_Init_Config.ts --network arbitrumGoerli --no-compile
-npx hardhat run deployments/16_migrate_Init_Token_1BAYC.ts --network arbitrumGoerli --no-compile
-npx hardhat run deployments/16_migrate_Init_Token_2MAYC.ts --network arbitrumGoerli --no-compile
-npx hardhat run deployments/17_migrate_AddLiquidity_1BAYC.ts --network arbitrumGoerli --no-compile
-npx hardhat run deployments/17_migrate_AddLiquidity_2MAYC.ts --network arbitrumGoerli --no-compile
-npx hardhat run deployments/18_migrate_SetPrice_1BAYC.ts --network arbitrumGoerli --no-compile
-npx hardhat run deployments/18_migrate_SetPrice_2MAYC.ts --network arbitrumGoerli --no-compile
+npx hardhat run deployments/1_migrate_Admin.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/2_migrate_PriceFeed_1BAYC.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/2_migrate_PriceFeed_2MAYC.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/2_migrate_PriceFeed_3CRYPTOPUNKS.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/2_migrate_PriceFeed_4MOONBIRD.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/2_migrate_PriceFeed_5AZUKI.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/2_migrate_PriceFeed_6CLONEX.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/2_migrate_PriceFeed_7DOODLE.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/2_migrate_PriceFeed_11BTC.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/3_migrate_Tokens.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/4_migrate_QuoteToken.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/5_migrate_BaseToken_0.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/5_migrate_BaseToken_1BAYC.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/5_migrate_BaseToken_2MAYC.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/5_migrate_BaseToken_3CRYPTOPUNKS.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/5_migrate_BaseToken_4MOONBIRD.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/5_migrate_BaseToken_5AZUKI.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/5_migrate_BaseToken_6CLONEX.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/5_migrate_BaseToken_7DOODLE.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/6_migrate_Library.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/6_migrate_UniswapV3.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/7_migrate_ClearingHouseConfig.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/8_migrate_MarketRegistry.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/9_migrate_OrderBook.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/10_migrate_AccountBalance.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/11_migrate_Exchange.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/12_migrate_InsuranceFund.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/13_migrate_Vault.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/14_migrate_CollateralManager.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/15_migrate_ClearingHouse.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/16_migrate_Init_Config.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/16_migrate_Init_Token_1BAYC.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/16_migrate_Init_Token_2MAYC.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/16_migrate_Init_Token_3CRYPTOPUNKS.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/16_migrate_Init_Token_4MOONBIRD.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/16_migrate_Init_Token_5AZUKI.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/16_migrate_Init_Token_6CLONEX.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/16_migrate_Init_Token_7DOODLE.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/17_migrate_AddLiquidity_1BAYC.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/17_migrate_AddLiquidity_2MAYC.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/17_migrate_AddLiquidity_3CRYPTOPUNKS.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/17_migrate_AddLiquidity_4MOONBIRD.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/17_migrate_AddLiquidity_5AZUKI.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/17_migrate_AddLiquidity_6CLONEX.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/17_migrate_AddLiquidity_7DOODLE.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/18_migrate_SetPrice_1BAYC.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/18_migrate_SetPrice_2MAYC.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/18_migrate_SetPrice_3CRYPTOPUNKS.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/18_migrate_SetPrice_4MOONBIRD.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/18_migrate_SetPrice_5AZUKI.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/18_migrate_SetPrice_6CLONEX.ts --network arbitrumTest --no-compile
+npx hardhat run deployments/18_migrate_SetPrice_7DOODLE.ts --network arbitrumTest --no-compile
 
-# npx hardhat run deployments/19_migrate_Console.ts --network arbitrumGoerli --no-compile
-# npx hardhat run deployments/20_migrate_Test.ts --network arbitrumGoerli --no-compile
-# npx hardhat run deployments/22_migrate_Faucet.ts --network arbitrumGoerli --no-compile
+
+# npx hardhat run deployments/19_migrate_Console.ts --network arbitrumTest --no-compile
+# npx hardhat run deployments/20_migrate_Test.ts --network arbitrumTest --no-compile
+# npx hardhat run deployments/22_migrate_Faucet.ts --network arbitrumTest --no-compile
+
+
