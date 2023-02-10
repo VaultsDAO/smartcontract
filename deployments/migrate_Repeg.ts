b769c7da544c0d1dbe7b3a72c22e618347f677f8
0xkongamoto <hieu.q@autonomous.nyc>
2023-02-10 08:55:03 +0700

    repeg

---
+++ deployments/migrate_Repeg.ts
@@ -0,0 +1,79 @@
+import fs from "fs";
+
+import bn from "bignumber.js"
+
+import hre, { ethers } from "hardhat";
+
+import { encodePriceSqrt, formatSqrtPriceX96ToPrice } from "../test/shared/utilities";
+import { AccountBalance, BaseToken, ClearingHouse, ClearingHouseConfig, CollateralManager, Exchange, InsuranceFund, MarketRegistry, MockPNFTToken, NftPriceFeed, OrderBook, QuoteToken, RewardMiner, TestFaucet, UniswapV3Pool, Vault } from "../typechain";
+import { getMaxTickRange } from "../test/helper/number";
+import helpers from "./helpers";
+import { formatEther, parseEther } from "ethers/lib/utils";
+const { waitForTx, tryWaitForTx } = helpers;
+
+
+async function main() {
+    await deploy();
+}
+
+export default deploy;
+
+async function deploy() {
+
+    const network = hre.network.name;
+    let fileName = process.cwd() + '/deployments/address/deployed_' + network + '.json';
+    let deployData: DeployData;
+    if (!(await fs.existsSync(fileName))) {
+        throw 'deployed file is not existsed'
+    }
+    let dataText = await fs.readFileSync(fileName)
+    deployData = JSON.parse(dataText.toString())
+
+    const [admin, maker, priceAdmin, platformFund, trader1, trader2, trader3, trader4, hieuq] = await ethers.getSigners()
+
+    // deploy UniV3 factory
+    var clearingHouseConfig = (await hre.ethers.getContractAt('ClearingHouseConfig', deployData.clearingHouseConfig.address)) as ClearingHouseConfig;
+    var marketRegistry = (await hre.ethers.getContractAt('MarketRegistry', deployData.marketRegistry.address)) as MarketRegistry;
+    var orderBook = (await hre.ethers.getContractAt('OrderBook', deployData.orderBook.address)) as OrderBook;
+    var accountBalance = (await hre.ethers.getContractAt('AccountBalance', deployData.accountBalance.address)) as AccountBalance;
+    var exchange = (await hre.ethers.getContractAt('Exchange', deployData.exchange.address) as Exchange);
+    var insuranceFund = (await hre.ethers.getContractAt('InsuranceFund', deployData.insuranceFund.address)) as InsuranceFund;
+    var vault = (await hre.ethers.getContractAt('Vault', deployData.vault.address)) as Vault;
+    var collateralManager = (await hre.ethers.getContractAt('CollateralManager', deployData.collateralManager.address)) as CollateralManager;
+    var clearingHouse = (await hre.ethers.getContractAt('ClearingHouse', deployData.clearingHouse.address)) as ClearingHouse;
+    var rewardMiner = (await hre.ethers.getContractAt('RewardMiner', deployData.rewardMiner.address)) as RewardMiner;
+    var pNFTToken = (await hre.ethers.getContractAt('MockPNFTToken', deployData.pNFTToken.address)) as MockPNFTToken;
+    var testFaucet = (await hre.ethers.getContractAt('TestFaucet', deployData.testFaucet.address)) as TestFaucet;
+
+    let baseTokens = [
+        deployData.vBAYC,
+        deployData.vMAYC,
+        deployData.vCRYPTOPUNKS,
+        deployData.vMOONBIRD,
+        deployData.vAZUKI,
+        deployData.vCLONEX,
+        deployData.vDOODLE,
+    ];
+    for (let i = 0; i < baseTokens.length; i++) {
+        let baseTokenAddr = baseTokens[i].address
+        var isAbleRepeg = (await clearingHouse.isAbleRepeg(baseTokenAddr))
+        console.log(
+            baseTokenAddr,
+            'isAbleRepeg',
+            isAbleRepeg
+        )
+        if (isAbleRepeg) {
+            await waitForTx(
+                await clearingHouse.repeg(baseTokenAddr),
+                'clearingHouse.repeg(' + baseTokenAddr + ')'
+            )
+        }
+    }
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
