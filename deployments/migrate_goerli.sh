033e1de49a1f91ebc0c8c5c24de026fcce4e068c
0xkongamoto <hieu.q@autonomous.nyc>
2023-02-12 15:20:04 +0700

    rf

--- deployments/migrate_goerli.sh
+++ deployments/migrate_goerli.sh
@@ -7,6 +7,5 @@ npx hardhat run deployments/migrate_3GetPrices.ts --network mainnet --no-compile
 npx hardhat run deployments/migrate_4SetPrices.ts --network arbitrumGoerli --no-compile
 npx hardhat run deployments/migrate_5InitVToken.ts --network arbitrumGoerli --no-compile
 npx hardhat run deployments/migrate_6AddLiquidity.ts --network arbitrumGoerli --no-compile
-npx hardhat run deployments/migrate_7Faucet.ts --network arbitrumGoerli --no-compile
 npx hardhat run deployments/migrate_8Trade.ts --network arbitrumGoerli --no-compile
 npx hardhat run deployments/migrate_9StartMiner.ts --network arbitrumGoerli --no-compile
